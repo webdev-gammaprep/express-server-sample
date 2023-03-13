7febd62a19b42d234df25a815a2caf5df42612c1
Anirudh J <anirudhjaishankar97@gmail.com>
2023-03-14 00:53:33 +0530

    Create logger and add logs for demo

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ const postRouter = require('./controller/posts');
 const userRouter = require('./controller/users');
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose');
-
-const { getAllBlogs } = require('./dao/blogs')
-
+const logger = require('./config/log');
 mongoose.connect('mongodb://127.0.0.1:27017/aniblog').then((mongoose) => {
   console.log('MongoDb Connected');
-  
 }).catch(err => {console.error(err)})
 
 app.use(cors());
@@ -20,9 +17,11 @@ app.use(express.json())
 app.post('/login', (req, res)=>{
   var user = users.find(u => u.username == req.body.username && u.password == req.body.password)
   if(user) {
+    logger.info(`User found for username: ${req.body.username}`);
     var token = jwt.sign({username: user.username, role: user.role}, 'secret');
     res.send(token)
   } else {
+    logger.error(`username: ${req.body.username} was not found!`)
     res.status(401).send('Auth failed');
   }
 })
